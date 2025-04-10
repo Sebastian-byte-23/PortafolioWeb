d3a2d00f14cbf6bd2391e44ac4ff12ff6fe5e793
Bysebax19 <surizar@cftla.cl>
2025-04-09 21:25:32 -0400

    primer

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+// Cargar Tailwind dinámicamente
+let tailwindScript = document.createElement("script");
+tailwindScript.src = "https://cdn.tailwindcss.com";
+document.head.appendChild(tailwindScript);
+
+// Función para cargar archivos HTML dentro de los divs
+async function includeHTML(id, file) {
+    let response = await fetch(file);
+    let text = await response.text();
+    document.getElementById(id).innerHTML = text;
+}
+
+// Cargar las secciones
+includeHTML("navbar", "sections/navbar.html");
+includeHTML("inicio", "sections/inicio.html");
+includeHTML("sobre-mi", "sections/sobre-mi.html");
+includeHTML("tecnologias", "sections/tecnologias.html");
+includeHTML("especializacion", "sections/especializacion.html");
+includeHTML("proyectos", "sections/proyectos.html");
+includeHTML("contacto", "sections/contacto.html");
